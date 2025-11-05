1117a55195bc3607a6a5329bf7fa135d205825ee
Azriel-Fajar <afw1407@gmail.com>
2025-11-05 15:12:47 +0700

    add transition

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,17 +2,18 @@ const navButtons = document.querySelectorAll(
   ".nav-menu ul li:not(.dropdown) a"
 );
 
+const dropBtn = document.getElementById("master-button");
+const dropContent = document.querySelector("#sidebar .dropdown");
+
 navButtons.forEach((button) => {
   button.addEventListener("click", function () {
     // Remove active class from all nav buttons
     navButtons.forEach((btn) => btn.classList.remove("active"));
     // Add active class to the clicked button
     this.classList.add("active");
+    dropContent.classList.remove("show");
   });
 });
-
-const dropBtn = document.getElementById("master-button");
-const dropContent = document.querySelector("#sidebar .dropdown");
 
 dropBtn.addEventListener("click", function () {
   dropContent.classList.toggle("show");
